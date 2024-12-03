d306d3ae38b2baf8360357d6f53ebc60d9b914b0
WayTwoOne <vladimirzakharovj@yandex.ru>
2024-12-03 05:25:37 -0800

    Initial Commit

---
+++ AnimeList/AnimeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeListApp.swift
+//  AnimeList
+//
+//  Created by Vladimir on 03.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimeList
+//
+//  Created by Vladimir on 03.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
